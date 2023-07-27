ccdce843e132a69964db991c1354834bb7193f8d
TeniG <tenigada@gmail.com>
2023-07-27 20:41:41 +0530

    - initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "Groceries Shop",
+        theme: ThemeData.dark().copyWith(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 147, 229, 250),
+            brightness: Brightness.dark,
+            surface: const Color.fromARGB(255, 42, 51, 59),
+          ),
+          scaffoldBackgroundColor: const Color.fromARGB(255, 50, 58, 60),
+        ),
+        home: Text("data"));
+  }
+}
